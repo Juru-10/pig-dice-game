7a37d0cf1d8ae9cdb6d59190bc86fc15f6bb917a
JURU <jurassu10@gmail.com>
2019-01-14 10:37:49 +0200

    add the variables in javascript

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var player1="";
+var player2="";
+var secondPlayer1="";
+var secondPlayer2="";
+
+var imgs1=["$('#img1')","$('#img2')","$('#img3')","$('#img4')","$('#img5')","$('#img6')"];
+var imgs2=["$('#img7')","$('#img8')","$('#img9')","$('#img10')","$('#img11')","$('#img12')"];
